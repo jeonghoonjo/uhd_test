3708e69a016ef0681e9af2453089a6280550c477
jeonghoon <akima95@naver.com>
2020-03-26 03:04:53 +0900

    2nd

--- git/src/fm.m
+++ git/src/fm.m
@@ -1,16 +1,16 @@
-fc = 4000;         %carrier freq
+%fc = 4000;         %carrier freq
+%fdev = 75000;    %frequency deviation
 fs = 5000000;       %sampling freq
 f = 1000;           %input signal freq
-fdev = 75000;    %frequency deviation
 t = (0 : 1/fs : 0.001-1/fs);
 
 x = cos(2*pi*f*t);  %input signal
-y = fmmod(x, fc, fs, fdev);
-complex_y = complex(y);
+%y = fmmod(x, fc, fs, fdev);
+%complex_y = complex(y);
 
 
 fileID = fopen('test.bin', 'w');
-fwrite(fileID, complex_y, 'float');
+fwrite(fileID, x, 'float');
 fclose(fileID);
 
 
--- git/src/test_usrp.cpp
+++ git/src/test_usrp.cpp
@@ -1,14 +1,17 @@
 #include <uhd/usrp/multi_usrp.hpp>
 #include <uhd/utils/safe_main.hpp>
+#include <uhd/utils/thread.hpp>
 #include <uhd/types/tune_request.hpp>
 #include <uhd/types/metadata.hpp>
+#include <uhd/types/dict.hpp>
 #include <uhd/stream.hpp>
 #include <uhd/convert.hpp>
-#include <Boost/format.hpp>
+#include <boost/format.hpp>
+#include <boost/thread/thread.hpp>
 #include <iostream>
 #include <fstream>
 #include <complex>
-#include <typeinfo>
+#include <csignal>
 
 using namespace std;
 
@@ -17,64 +20,39 @@ static bool stop_signal_called = false;
 void sig_int_handler(int){
 
 	stop_signal_called = true;
-
 }
 
 void send_from_file(
 	uhd::tx_streamer::sptr tx_streamer,
-	const string& file,
-	size_t samps_per_buff/*,
-	uhd::convert::converter::sptr converter*/)
+	const string&	 file,
+	size_t samps_per_buff)
 {
-	int count(0);
-	size_t num_tx_samps_;
+
 	uhd::tx_metadata_t md;
 	md.start_of_burst = false;
 	md.end_of_burst = false;
-	samps_per_buff = 3000 ; 
-	vector<complex<float>> input_buff(samps_per_buff);
-	vector<complex<int>> output_buff(samps_per_buff);
+	vector<complex<float>> buff(samps_per_buff); 
+	size_t num_tx_samps, num_tx_samps_;
 	
-	ifstream infile(file, ifstream::binary);
+	ifstream infile(file.c_str(), ifstream::binary);
 
-	while(not stop_signal_called and not md.end_of_burst){
+	while(not md.end_of_burst){
 		
-		infile.read((char*)&input_buff.front(), input_buff.size()*sizeof(complex<float>));
-
-		size_t num_tx_samps = size_t(infile.gcount() / sizeof(complex<float>));
-
-		//converter->conv(input_buff, output_buff, num_tx_samps);
-
-		/*for(int i= 0; i < num_tx_samps; i++){
-			cout << input_buff[i] << endl;
-			count ++;
-		}		
-		cout << count << endl;
-		count = 0;
-		*/
+		infile.read((char*)&buff.front(), buff.size()*sizeof(complex<float>));
+		num_tx_samps = size_t(infile.gcount() / sizeof(complex<float>));
 		
-
 		md.end_of_burst = infile.eof();
+		
+		num_tx_samps_ = tx_streamer->send(&buff.front(), num_tx_samps, md);
 
-		num_tx_samps_ = tx_streamer->send(&input_buff.front(), num_tx_samps, md);
-
-		//cout << "num_tx_samps : " << num_tx_samps_ << endl;
+		cout << num_tx_samps_ << endl;
+				
+	}
 	
-	}	
+	infile.close();	
 
 }
 
-uhd::convert::converter::sptr make_converter(){
-	
-	uhd::convert::id_type converter_id;
-	converter_id.input_format  = "fc32"  ;
-	converter_id.output_format = "sc16"  ;  
-	converter_id.num_inputs    = 3000    ; //??
-	converter_id.num_outputs   = 3000    ; //??
-
-	return get_converter(converter_id, 0)();
-
-}
 
 int UHD_SAFE_MAIN(int argc, char* argv[])
 {
@@ -83,36 +61,23 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
 	string ant 	      ("TX/RX")		  ;
 	string cpu_format     ("fc32")		  ;
 	string wire_format    ("sc16")		  ;
-	string file 	      ("final_test.bin")  ;
-	size_t samps_per_buff			  ;
+	string file 	      ("test.bin")        ;
+	size_t samps_per_buff ( 3000 )		  ;
 	size_t chan 	      ( 0 )          	  ;
 	size_t mboard 	      ( 0 )	       	  ;
-	double gain 	      ( 40.0 )         	  ;
+	double gain 	      ( 73.2 )         	  ;
 	double rate 	      ( 5.0e6 )      	  ;
 	double master_rate    ( 40.0e6 )    	  ;   	 
 	double freq 	      ( 2.6e9 )		  ;
-	double bw	      ( 10.0e6 )	  ;
+	double bw	      ( 0.2e6 )	  	  ;
 	double num_samps      ( 5000 )		  ;
 	bool   repeat	      ( 1 )		  ;
 	vector<string> filter_names;
-	/*
-	//set converter	
-	uhd::convert::id_type converter_id;
-	converter_id.input_format  = cpu_format  ;
-	converter_id.output_format = wire_format  ;  
-	converter_id.num_inputs    = 3000    ; //??
-	converter_id.num_outputs   = 3000    ; //??
-
-	uhd::convert::register_converter(converter_id, make_converter, 0);
-
-	uhd::convert::converter::sptr converter = make_converter();	
-	converter->set_scalar(32767.); // fc32 -> sc16
-	*/
 
 	uhd::tune_request_t tune_request (freq);
 	uhd::stream_args_t stream_args(cpu_format, wire_format);
 
-
+	std::signal(SIGINT, &sig_int_handler);
 	//make a new multi usrp 
 	uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(dev_args);
 
@@ -146,29 +111,12 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
 	usrp->set_tx_bandwidth(bw, chan);
 	cout << boost::format("TX bandwidth : %lf MHz") % (usrp->get_tx_bandwidth(chan) / 1e6 ) << endl;
 	
-	//NOT YET//
-	//set filter 
-	filter_names = usrp->get_filter_names();
-	
-	for(int i = 0; i < filter_names.size(); i++){
-	
-		cout << boost::format("%s") % (filter_names[i]) << endl;
-	}
 
-
-	cout << boost::format("The # of tx channels: %zu") % (usrp->get_tx_num_channels()) << endl;
-	cout << boost::format("The # of rx channels: %zu") % (usrp->get_rx_num_channels()) << endl;
-
-	
 	//make tx streamer
 	uhd::tx_streamer::sptr tx_streamer = usrp->get_tx_stream(stream_args);
 	
 	while(not stop_signal_called){
-	//while(repeat){
-		send_from_file(tx_streamer, file, samps_per_buff/*, converter*/);
-		//repeat = 0;
+		send_from_file(tx_streamer, file, samps_per_buff);
 	}
-
-	
 	return EXIT_SUCCESS;
 }
